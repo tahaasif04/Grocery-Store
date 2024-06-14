e99b50298d384ff43b769be0e67c8b4e58fcad88
tahaasif04 <114299535+tahaasif04@users.noreply.github.com>
2024-06-14 04:18:53 -0700

    Add files via upload

--- Grocery/js/script.js
+++ Grocery/js/script.js
@@ -5,3 +5,9 @@ document.querySelector('#search-btn').onclick = () =>
     searchForm.classList.toggle('active');
 }
 
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>
+{
+    shoppingCart.classList.toggle('active');
+}
