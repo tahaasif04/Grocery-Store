1fbf755b4b11dcab545f1c7ae3d8165c73b5ece7
tahaasif04 <114299535+tahaasif04@users.noreply.github.com>
2024-06-21 22:39:54 +0500

    login form

--- Grocery/js/script.js
+++ Grocery/js/script.js
@@ -11,3 +11,11 @@ document.querySelector('#cart-btn').onclick = () =>
 {
     shoppingCart.classList.toggle('active');
 }
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick= () =>
+{
+
+    loginForm.classList.toggle('active');
+}
